e8afce918a4765084ffd07f613ecc3a0e02bbde0
James Bourbeau <jrbourbeau@users.noreply.github.com>
2020-09-14 18:38:02 -0500

    Fix data download prep (#24)

--- prep.py
+++ prep.py
@@ -3,21 +3,14 @@ import sys
 import argparse
 import os
 from glob import glob
-import json
-import gzip
 import tarfile
 import urllib.request
 
-import h5py
-import numpy as np
 import pandas as pd
-from skimage.transform import resize
-
-from accounts import account_entries, account_params, json_entries
 
 import sources
 
-DATASETS = ["random", "weather", "accounts", "flights", "all"]
+DATASETS = ["flights", "all"]
 here = os.path.dirname(__file__)
 data_dir = os.path.abspath(os.path.join(here, 'data'))
 
@@ -90,121 +83,6 @@ def flights(small=None):
     end = time.time()
     print("** Created flights dataset! in {:0.2f}s**".format(end - start))
 
-def random_array(small=None):
-    if small is None:
-        small = bool(os.environ.get("DASK_TUTORIAL_SMALL", False))
-
-    if small:
-        blocksize = 5000
-    else:
-        blocksize = 1000000
-
-    nblocks = 1000
-    shape = nblocks * blocksize
-
-    t0 = time.time()
-    if os.path.exists(os.path.join(data_dir, 'random.hdf5')):
-        return
-
-    with h5py.File(os.path.join(data_dir, 'random.hdf5'), mode='w') as f:
-        dset = f.create_dataset('/x', shape=(shape,), dtype='f4')
-        for i in range(0, shape, blocksize):
-            dset[i: i + blocksize] = np.random.exponential(size=blocksize)
-
-    t1 = time.time()
-    print("Created random data for array exercise in {:0.2f}s".format(t1 - t0))
-
-
-def accounts_csvs(small=None):
-    t0 = time.time()
-    if small is None:
-        small = bool(os.environ.get("DASK_TUTORIAL_SMALL", False))
-
-    if small:
-        num_files, n, k = 3, 10000, 100
-    else:
-        num_files, n, k = 3, 1000000, 500
-
-    fn = os.path.join(data_dir, 'accounts.%d.csv' % (num_files - 1))
-
-    if os.path.exists(fn):
-        return
-
-    args = account_params(k)
-
-    for i in range(num_files):
-        df = account_entries(n, *args)
-        df.to_csv(os.path.join(data_dir, 'accounts.%d.csv' % i),
-                  index=False)
-
-    t1 = time.time()
-    print("Created CSV acccouts in {:0.2f}s".format(t1 - t0))
-
-
-def accounts_json(small=None):
-    t0 = time.time()
-    if small is None:
-        small = bool(os.environ.get("DASK_TUTORIAL_SMALL", False))
-
-    if small:
-        num_files, n, k = 50, 10000, 250
-    else:
-        num_files, n, k = 50, 100000, 500
-    fn = os.path.join(data_dir, 'accounts.%02d.json.gz' % (num_files - 1))
-    if os.path.exists(fn):
-        return
-
-    args = account_params(k)
-
-    for i in range(num_files):
-        seq = json_entries(n, *args)
-        fn = os.path.join(data_dir, 'accounts.%02d.json.gz' % i)
-        with gzip.open(fn, 'wb') as f:
-            f.write(os.linesep.join(map(json.dumps, seq)).encode())
-
-    t1 = time.time()
-    print("Created JSON acccouts in {:0.2f}s".format(t1 - t0))
-
-
-def create_weather(small=None):
-    t0 = time.time()
-    if small is None:
-        small = bool(os.environ.get("DASK_TUTORIAL_SMALL", False))
-
-    if small:
-        growth = 1
-    else:
-        growth = 32
-    filenames = sorted(glob(os.path.join(data_dir, 'weather-small', '*.hdf5')))
-
-    if not filenames:
-        ws_dir = os.path.join(data_dir, 'weather-small')
-        raise ValueError('Did not find any hdf5 files in {}'.format(ws_dir))
-
-    if not os.path.exists(os.path.join(data_dir, 'weather-big')):
-        os.mkdir(os.path.join(data_dir, 'weather-big'))
-
-    if all(os.path.exists(fn.replace('small', 'big')) for fn in filenames):
-        return
-
-    for fn in filenames:
-        with h5py.File(fn, mode='r') as f:
-            x = f['/t2m'][:]
-
-        if small:
-             y = x
-             chunks = (180, 180)
-        else:
-            y = resize(x, (x.shape[0] * growth, x.shape[1] * growth), mode='constant')
-            chunks = (500, 500)
-
-        out_fn = os.path.join(data_dir, 'weather-big', os.path.split(fn)[-1])
-
-        with h5py.File(out_fn, mode='w') as f:
-            f.create_dataset('/t2m', data=y, chunks=chunks)
-    t1 = time.time()
-    print("Created weather dataset in {:0.2f}s".format(t1 - t0))
-
 
 def main(args=None):
     args = parse_args(args)
@@ -215,13 +93,6 @@ def main(args=None):
         ssl._create_default_https_context = ssl._create_unverified_context
         print("done", flush=True)
 
-    if args.dataset == "random" or args.dataset == "all":
-        random_array(args.small)
-    if args.dataset == "weather" or args.dataset == "all":
-        create_weather(args.small)
-    if args.dataset == "accounts" or args.dataset == "all":
-        accounts_csvs(args.small)
-        accounts_json(args.small)
     if args.dataset == "flights" or args.dataset == "all":
         flights(args.small)
 
